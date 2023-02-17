c85fbc3a673de5b910dbb7f20304e38f45ff648d
G. Godwin <g.godwin@Gs-MacBook-Pro.local>
2023-02-16 19:44:50 -0500

    comment controller post and get working

--- controllers/api/comment.js
+++ controllers/api/comment.js
@@ -1,5 +1,16 @@
 const router = require('express').Router();
-const { Comment } = require('../../models'); 
+const { Comment, Postdata } = require('../../models'); 
+
+router.get('/', async (req, res) => {
+  try {
+    const commentData = await Comment.findAll({
+      include: [Postdata] 
+    });
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 router.post('/', async (req, res) => {
     try {
--- seeds/postData.js
+++ seeds/postData.js
@@ -4,16 +4,19 @@ const postdata = [
   {
     title:'Why is MVC so important?',
     content:'MVC allows developers to maintain a true separation of concerns, devising their code between the Model layer for data, the View layer for design, and the Controller layer for application logic.',
+    user_id: 1,
     comment_id: 1,
   },
   {
     title: 'Placeholder 1',
     content: 'Lorem Ipsum is Lorem Ipsum',
+    user_id: 1,
     comment_id: 2,  
   }, 
   {
     username: 'Placeholder 2',
     post_date: 'Lorem Ipsum is Lorem Ipsum',
+    user_id: 2,
     comment_id: 3,
   },
 ];
