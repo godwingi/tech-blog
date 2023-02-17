1aa7575ad66b988182c02e7dc164bd05d407c85f
G. Godwin <g.godwin@Gs-MacBook-Pro.local>
2023-02-16 19:52:05 -0500

    home route get functional

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -7,10 +7,7 @@ router.get('/', async (req, res) => {
     try {
         const postData = await Postdata.findAll();
         const posts = postData.map((post) => post.get({ plain: true }));
-        res.render('homepage', {
-            posts,
-            // logged_in: req.session.logged_in,
-        });
+        res.status(200).json(posts);
     } catch (err) {
         res.status(500).json(err);
     }
