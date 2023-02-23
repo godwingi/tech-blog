7e2a55865938ec537227f8043359a219a717740e
G. Godwin <g.godwin@Gs-MacBook-Pro.local>
2023-02-23 14:56:15 -0500

    Handlebar request added to server, CSS added

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
+// Dependencies
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
+// Import express handlebards
+const expressHandlebars = require('express-handlebars');
+const handlebarsInstance = expressHandlebars.create({});
+const path = require('path');
+
+// Sets up Express
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Middleware configuration
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Defining template engine
+app.engine('handlebars', handlebarsInstance.engine);
+app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
+// Listening to port
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
