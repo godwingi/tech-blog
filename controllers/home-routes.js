a42a257f68933b152c6bd4a2faa9ff3a334551a8
G. Godwin <g.godwin@Gs-MacBook-Pro.local>
2023-02-23 19:18:15 -0500

    figure out #each, but not populating, but is each-ing

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,36 +1,21 @@
 const router = require('express').Router();
-const { Postdata } = require('../models');
+const { Postdata , Comment, User } = require('../models');
 // const withAuth = require('../utils/auth');
 // const { Op } = require('sequelize');
 
-// const postdata = require('../models/postdata');
-
-// router.get('/:id', async (req, res) => {
-//     try {
-//         const postData = await Postdata.findAll();
-//         const posts = postData.map((post) => post.get({ plain: true }));
-//         res.render('homepage', posts[req.params.id - 1]);
-//               } catch (err) {
-//                 res.status(500).json(err);
-//               }
-//             });
-
-// router.get('/', async (req, res) => {
-//     try {
-//         const postData = await Postdata.findAll();
-//         const posts = postData.map((post) => post.get({ plain: true }));
-     
-//         res.render('homepage', {
-//                   posts,
-//                 });
-//               } catch (err) {
-//                 res.status(500).json(err);
-//               }
-//             });
-
-// router.get('/:num', async (req, res) => {
-//   return res.render('homepage', postdata[req.params.num -1]);
-// });
-
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Postdata.findAll({
+          include: [Comment, User]
+        });
+        const posts = postData.map((post) => post.get({ plain: true }));
+        console.log('this is the console log of POSTS --------> ', posts);
+        res.render('homepage', {
+                  posts,
+                });
+              } catch (err) {
+                res.status(500).json(err);
+              }
+            });          
 
 module.exports = router;
